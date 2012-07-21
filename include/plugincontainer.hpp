d9d0fcf20d85ca5bfd83e580c2f077460a71ede4
Minh Ngo <nlminhtl@gmail.com>
2012-07-21 12:43:37 +0300

    Plugin container

---
+++ include/plugincontainer.hpp
@@ -0,0 +1,51 @@
+/*
+ * kdots
+ * Copyright (C) 2011-2012 Minh Ngo <nlminhtl@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef KDOTS_PLUGINCONTAINER_HPP
+#define KDOTS_PLUGINCONTAINER_HPP
+#include <QMap>
+#include <interface/iplugin.hpp>
+
+namespace KDots
+{
+  class PluginContainer
+  {
+    typedef QMap<QString, IPlugin*> PluginMap;
+    PluginMap m_pluginMap;
+  public:
+    PluginContainer () {}
+    static PluginContainer& instance ();
+    
+    inline const PluginMap&
+    plugins () const
+    {
+      return m_pluginMap;
+    }
+    
+    inline IPlugin*
+    plugin (const QString& name)
+    {
+      PluginMap::const_iterator itr = m_pluginMap.find (name);
+      return itr != m_pluginMap.end () ? *itr : NULL;
+    }
+    
+  private:
+    void loadPlugins ();
+  };
+}
+
+#endif
---
+++ plugincontainer.cpp
@@ -0,0 +1,57 @@
+/*
+ * kdots
+ * Copyright (C) 2011-2012 Minh Ngo <nlminhtl@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "plugincontainer.hpp"
+#include <QDebug>
+#include <QDir>
+#include <QApplication>
+#include <QPluginLoader>
+#include "interface/iplugin.hpp"
+#include "constants.hpp"
+
+namespace KDots
+{
+  PluginContainer&
+  PluginContainer::instance ()
+  {
+    static PluginContainer obj;
+    return obj;
+  }
+  
+  void
+  PluginContainer::loadPlugins ()
+  {
+    //TODO: Plugins must be situated in the system unix directory
+    QDir pluginsDir (qApp->applicationDirPath ());
+    pluginsDir.cd ("plugins");
+    
+    for (const QString& fileName : pluginsDir.entryList (QStringList ({PLUGIN_SUFFIX + "*"}), QDir::Files))
+      {
+        QPluginLoader pluginLoader (pluginsDir.absoluteFilePath (fileName));
+        QObject *plugin = pluginLoader.instance ();
+        if (plugin)
+          {
+            IPlugin *iplugin = qobject_cast<IPlugin*> (plugin);
+            if (iplugin)
+              {
+                qDebug () << Q_FUNC_INFO << "Loading the plugin:" << iplugin->name ();
+                m_pluginMap.insert (iplugin->name (), iplugin);
+              }
+          }
+      }
+  }
+}
