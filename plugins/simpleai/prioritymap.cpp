98d803e0c3dd3a39ad172ec9f1a5a2468c436539
Minh Ngo <nlminhtl@gmail.com>
2015-03-24 02:09:11 +0200

    A bit more refactoring for the PriorityMap

--- plugins/simpleai/prioritymap.cpp
+++ plugins/simpleai/prioritymap.cpp
@@ -1,7 +1,7 @@
 /*
  * KDots
  * Copyright (c) 2011, 2012, 2014, 2015 Minh Ngo <minh@fedoraproject.org>
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -27,6 +27,7 @@
 #include <config.hpp>
 
 #include <boost/bimap.hpp>
+#include <boost/range/combine.hpp>
 
 #include <KDebug>
 
@@ -48,25 +49,25 @@ namespace KDots
       {MapElement::PS, "PS"},
       {MapElement::CU, "CU"}
     };
-    
+
     const MapElementToStrType MAP_ELEMENT_TO_STR(MAP_ELEMENT_TO_STR_VALUES.begin(),
                                                  MAP_ELEMENT_TO_STR_VALUES.end());
-    
+
     MapData::MapData()
     {
     }
-    
+
     MapData::MapData(const MapType& map, const Point& current, float priority)
       : m_map(map)
       , m_current(current)
       , m_priority(priority)
     {
     }
-    
+
     QString MapData::toString() const
     {
       QString res;
-      
+
       for (const auto& row : m_map)
       {
         res += "\n{ ";
@@ -76,23 +77,23 @@ namespace KDots
         }
         res += "}";
       }
-        
+
       res += "\n";
-      
+
       return res;
     }
-    
+
     bool MapData::operator==(const MapData& other) const
     {
       if (m_current != other.m_current)
         return false;
-      
+
       if (m_priority != other.m_priority)
         return false;
-      
+
       if (other.m_map.size() != m_map.size() || !other.m_map.size() || !m_map.size())
         return false;
-      
+
       for (MapType::const_iterator other_itr = other.m_map.begin(),
            itr = m_map.begin(), other_e = other.m_map.end();
            other_itr != other_e ; ++other_itr, ++itr)
@@ -100,92 +101,91 @@ namespace KDots
         if (!std::equal(other_itr->begin(), other_itr->end(), itr->begin()))
           return false;
       }
-      
+
       return true;
     }
-    
+
     bool MapData::operator!=(const MapData& other) const
     {
       return !(other == *this);
     }
-    
+
     MapData PriorityMap::inverse(const MapData& data)
     {
       const MapType& map = data.m_map;
       MapData newData;
       MapType& newMap = newData.m_map;
-      
+
       newMap.resize(map.size());
       
-      MapType::const_iterator map_i = map.begin();
-      for (MapType::iterator new_i = newMap.begin(),
-          new_e = newMap.end();
-          new_i != new_e; ++new_i, ++map_i)
+      for (const boost::tuple<const MapLine&, MapLine&>& tup : boost::combine(map, newMap))
       {
-        new_i->resize(map_i->size());
-        std::reverse_copy(map_i->begin(), map_i->end(), new_i->begin());
+        MapLine& newLine = tup.get<1>();
+        const MapLine& oldLine = tup.get<0>();
+        
+        newLine.resize(oldLine.size());
+        std::reverse_copy(oldLine.begin(), oldLine.end(), newLine.begin());
       }
-      
+
       newData.m_priority = data.m_priority;
       newData.m_current = {static_cast<int>(map.front().size() - 1 - data.m_current.m_x),
                            data.m_current.m_y};
-      
+
       return newData;
     }
-    
-    MapData PriorityMap::rotate(const MapData& data) 
+
+    MapData PriorityMap::rotate(const MapData& data)
     {
       const MapType& map = data.m_map;
       MapData newData;
       MapType& newMap = newData.m_map;
-      
+
       newMap.resize(map.front().size());
-      std::for_each(newMap.begin(), newMap.end(), [&map](MapLine& line) {
+      for (MapLine& line : newMap)
         line.resize(map.size());
-      });
-      
+
       for (int i = 0, max_i = newMap.size(), j, max_j = map.size(); i != max_i; ++i) //y
       {
         for (j = 0; j != max_j; ++j) //x
-          newMap[i][j] = map[max_j - 1 - j][i]; 
+          newMap[i][j] = map[max_j - 1 - j][i];
       }
-      
+
       newData.m_priority = data.m_priority;
       newData.m_current = {static_cast<int>(map.size() - 1 - data.m_current.m_y),
                            data.m_current.m_x};
-      
+
       return newData;
     }
-    
+
     PriorityMap::PriorityMap()
       : m_priorityMap(loadMap())
     {
     }
-    
+
     PriorityMap& PriorityMap::instance()
     {
       static PriorityMap obj;
       return obj;
     }
-    
+
     const QVector<MapData>& PriorityMap::priorityMap()
     {
       return m_priorityMap;
     }
-    
+
     QVector<MapData> PriorityMap::loadMap() const
     {
       QVector<MapData> priorityMap;
-      
+
       QFile file(PLUGINS_DIR "/rules");
       file.open(QIODevice::ReadOnly);
-      
+
       QTextStream in(&file);
-      
+
       while (!in.atEnd())
       {
         const float priority = in.readLine().toFloat();
-        
+
         Point current;
         MapType map;
         for (int row = 0;; ++row)
@@ -193,31 +193,31 @@ namespace KDots
           const QString& line = in.readLine();
           if (line[0] == '/')
             break;
-          
+
           const QStringList& cells = line.split(' ');
           map.push_back(MapLine(cells.size()));
           MapLine& lastRow = map.back();
           for (int column = 0; column < cells.size(); ++column)
           {
             const MapElement type = MAP_ELEMENT_TO_STR.right.at(cells[column]);
-            
+
             lastRow[column] = type;
-            
+
             if (type == MapElement::CU)
               current = {column, row};
           }
         }
-        
+
         priorityMap.push_back(MapData(std::move(map), current, priority));
       }
-      
+
       priorityMap.reserve(priorityMap.size() * 4);
       for (int i = 0, max = priorityMap.size(); i < max; ++i)
       {
         const MapData& data = priorityMap[i];
         const MapData& invertedData = inverse(data);
         priorityMap.push_back(invertedData);
-        
+
         MapData newData = data;
         for (int j = 0; j < 3; ++j)
         {
@@ -227,7 +227,7 @@ namespace KDots
           priorityMap.push_back(invertedMap);
         }
       }
-      
+
       return priorityMap;
     }
   }
